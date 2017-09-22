28f43766d04e7a73c99650af4ed4e801f8dd27ac
si-wu-physics <wusi1983@gmail.com>
2017-09-22 14:07:18 -0700

    add constants

---
+++ constants.hpp
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+namespace Calculator
+{
+  void addConstantsToVariableMap( const std::string& name,
+                                  double value,
+                                  var_map_t& varMap );
+}
+
+
+#endif
